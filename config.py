feb2db26fc0d1e800787864a259ec8c99750f339
dozapsixoza1 <166742425+dozapsixoza1@users.noreply.github.com>
2026-04-26 21:38:08 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 BOT_TOKEN = "8721060352:AAH8hLCMGWCjk_hwCCwsphLedmT-Em11EoY"
-ADMIN_IDS = [7950038145]
+ADMIN_IDS = [7950038145]  # твой Telegram ID
 
+# Каналы: имя, ссылка, числовой ID (обязательно для проверки)
 REQUIRED_CHANNELS = [
-    {"name": "FL Osint", "url": "https://t.me/+TSQqyiX1DPVlY2Qy"},
-    {"name": "psychos", "url": "https://t.me/+W4dtlB6y511hM2Zi"}
+    {"name": "FL Osint", "url": "https://t.me/+TSQqyiX1DPVlY2Qy", "id": -1003630830270},
+    {"name": "psychos", "url": "https://t.me/+W4dtlB6y511hM2Zi", "id": -1003289272076}
 ]
 
 PRICES = {2:20, 4:50, 15:200, 30:500, 50:1000, 120:3000, 600:20000}
